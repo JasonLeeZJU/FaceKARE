29fb2b3d13d8410bb68a4683e9cd3f4f115f3175
Anan <anan@AnandeMacBook-Pro.local>
2017-04-25 13:31:36 +0800

    Initial Commit

---
+++ FaceKARE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceKARE
+//
+//  Created by Anan on 2017/4/25.
+//  Copyright © 2017年 Anan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
